fixing bounds, fixing numerical aspects, adding translate objective functionality

src/minlp.py
@@ -1,5 +1,5 @@
 import cyipopt
-import numpy as np
+import autograd.numpy as np
 from autograd import grad, jacobian
 import autograd.numpy as anp # TODO use instead of numpy if autograd is failing
 
@@ -23,6 +23,7 @@ class MRTA_XD():
         self.in_nbrs = []
         for curr_node in range(self.num_tasks):
             self.in_nbrs.append([n for n in self.task_graph.predecessors(curr_node)])
+        self.jacobian_handle = jacobian(self.constraints)
 
     def objective(self, x):
         """Returns the scalar value of the objective given x."""
@@ -31,21 +32,25 @@ class MRTA_XD():
         x_ak = np.reshape(np.array(x_ak), (self.num_robots, self.num_tasks + 1)) # reshape so each row contains x_ak for agent a
         x_dummy = x_ak[:,0]
         x_ak = x_ak[:,1:]
-        task_coalitions = np.zeros((self.num_tasks,)) # list of coalition size assigned to each task
-        task_coalition_rewards = np.zeros((self.num_tasks,))
+        task_coalitions = []#np.zeros((self.num_tasks,)) # list of coalition size assigned to each task
+        task_coalition_rewards = [] # np.zeros((self.num_tasks,))
         for t in range(self.num_tasks):
-            task_coalitions[t] = np.sum(x_ak[:,t])/self.num_robots
-            task_coalition_rewards[t] = self.reward_model._compute_node_coalition(t,task_coalitions[t])
+            try:
+                task_coalitions.append(np.sum(x_ak[:,t])/self.num_robots)
+            except(ValueError):
+                breakpoint()
+            task_coalition_rewards.append(self.reward_model._compute_node_coalition(t,task_coalitions[t]))
 
 
         tasks_ordered = np.argsort(np.array(f_k))
-        task_rewards = np.zeros((self.num_tasks,))
+        task_rewards = [] # np.zeros((self.num_tasks,))
         for t in tasks_ordered:
             task_reward, _ = self.reward_model.compute_node_reward_dist(t,task_coalition_rewards[t],[task_rewards[k] for k in self.in_nbrs[t]], np.zeros_like(task_rewards))
-            task_rewards[t] = task_reward
+            task_rewards.append(task_reward)
 
-        print('task coalitions: ', task_coalitions)
-        print('task_rewards: ', task_rewards)
+        #print('task coalitions: ', task_coalitions)
+        #print('task_rewards: ', task_rewards)
+        #print(x)
 
         return np.sum(task_rewards)
 
@@ -59,43 +64,39 @@ class MRTA_XD():
         # ])
         if self.gradient_func == None:
             self.gradient_func = grad(self.objective)
+        #print(np.array(self.gradient_func(x)))
         return np.array(self.gradient_func(x))
 
     def constraints(self, x):
         """Returns the constraints."""
         x_ak, o_akk, z_ak, s_k, f_k = self.partition_x(x)
         x_ak = np.reshape(np.array(x_ak), (self.num_robots, self.num_tasks + 1)) # reshape so each row contains x_ak for agent a
-        x_dummy = x_ak[:,0]
-        x_ak = x_ak[:,1:]
+        z_ak = np.reshape(np.array(z_ak),(self.num_robots, self.num_tasks + 1))
 
         # reshape o_akk so that o_akk[a, k-1, k'] = 1 --> agent a performs task k' immediately after task k
         # index 0 in dimension 2 is for the dummy tasks. duplicates not included for dummy tasks, but included for all others
-        o_akk = np.reshape(np.array(o_akk), (self.num_robots, self.num_tasks+1, self.num_tasks-1))
-
+        o_akk = np.reshape(np.array(o_akk), (self.num_robots, self.num_tasks+1, self.num_tasks))
 
         constraints = []
 
         # constraint a: every agent starts with one dummy task -- equal to 1
         for a in range(self.num_robots):
-            constraints.append(x_dummy[a])
+            constraints.append(x_ak[a,0] - 1)
 
         # constraint d: every task an agent performs has exactly one predecessor
         for a in range(self.num_robots):
             for k_p in range(self.num_tasks):
-                constraints.append(np.sum(o_akk[a,:,k_p])-x_ak[a,k_p])
-
+                constraints.append(np.sum(o_akk[a,:,k_p])-x_ak[a,k_p+1])
         # constraint e: every task an agent performs has exactly one successor except the last task
         for a in range(self.num_robots):
             for k in range(self.num_tasks+1): #K_a+
-                constraints.append(np.sum(o_akk[a,k,:]) + z_ak[a,k] - x[a,k])
+                constraints.append(np.sum(o_akk[a,k,:]) + z_ak[a,k] - x_ak[a,k])
 
-
-
-        return np.array((np.prod(x), np.dot(x, x)))
+        return np.array(constraints)
 
     def jacobian(self, x):
         """Returns the Jacobian of the constraints with respect to x."""
-        return np.concatenate((np.prod(x)/x, 2*x))
+        return np.array(self.jacobian_handle(x))
 
     # def hessianstructure(self):
     #     """Returns the row and column indices for non-zero vales of the
@@ -136,12 +137,12 @@ class MRTA_XD():
 
         msg = "Objective value at iteration #{:d} is - {:g}"
 
-        print(msg.format(iter_count, obj_value))
+        print(msg.format(iter_count, obj_value), "\n", )
 
     def partition_x(self, x):
         x_len = (self.num_tasks+1)*self.num_robots # extra dummy task
         o_len = self.num_robots*((self.num_tasks+1)*(self.num_tasks)) #extra dummy task, KEEP duplicates
-        z_len = self.num_tasks*self.num_robots #each agent can finish on each task
+        z_len = (self.num_tasks + 1)*self.num_robots #each agent can finish on each task -- include dummy task, as agents can do 0 tasks
         s_len = self.num_tasks
         f_len = self.num_tasks
         x_ak = x[0:x_len]

src/taskgraph.py
@@ -3,6 +3,7 @@ from pydrake.solvers.mathematicalprogram import MathematicalProgram
 from pydrake.solvers.mathematicalprogram import Solve
 import pydrake.math as math
 from scipy.optimize import minimize, LinearConstraint
+import cyipopt
 
 import matplotlib.pyplot as plt
 import matplotlib
@@ -547,12 +548,12 @@ class TaskGraph:
 
     def test_minlp(self):
 
-        case_a = False
-        case_b = True
+        case_a = True
+        case_b = False
         if case_a:
             x_ak = np.zeros(((self.num_tasks+1)*self.num_robots,))
             o_akk = np.zeros((self.num_robots,self.num_tasks+1, self.num_tasks),)
-            z_ak = np.zeros((self.num_tasks*self.num_robots,))
+            z_ak = np.zeros(((self.num_tasks+1)*self.num_robots,))
             s_k = 2*np.arange(self.num_tasks)
             f_k = s_k+1
 
@@ -572,10 +573,37 @@ class TaskGraph:
             x_vec = np.concatenate((x_ak, o_akk.flatten(), z_ak,s_k,f_k))
             self.minlp_obj.objective(x_vec)
 
+            lb = [0.0 for _ in range(len(x_vec))]
+            ub_xoz = [1.0 for _ in range(len(x_ak) + len(o_akk.flatten()) + len(z_ak))]
+            ub_sf = [100 for _ in range(len(s_k)+len(f_k))]
+            ub = ub_xoz + ub_sf
+
+
+            curr_n_constraints = self.num_robots* (1 + self.num_tasks + (self.num_tasks+1))
+            cl = [0.0 for _ in range(curr_n_constraints)]
+            cu = cl
+
+            #breakpoint() #NOTE: currently seems to barely move, staying near initial condition
+            x0 = x_vec*0.5
+            nlp = cyipopt.problem(
+                n=len(x_vec),
+                m=curr_n_constraints,
+                problem_obj=self.minlp_obj,
+                lb=lb,
+                ub=ub,
+                cl=cl,
+                cu=cu
+            )
+            nlp.addOption('max_iter', 10000)
+            nlp.addOption('print_level', 8)
+            x, info = nlp.solve(x0)
+            self.translate_minlp_objective(x)
+            breakpoint()
+
         if case_b:
             x_ak = np.zeros(((self.num_tasks+1)*self.num_robots,))
             o_akk = np.zeros((self.num_robots,self.num_tasks+1, self.num_tasks),)
-            z_ak = np.zeros((self.num_tasks*self.num_robots,))
+            z_ak = np.zeros(((self.num_tasks+1)*self.num_robots,))
             s_k = 2*np.arange(self.num_tasks)
             f_k = s_k+1
 
@@ -608,6 +636,28 @@ class TaskGraph:
             x_vec = np.concatenate((x_ak, o_akk.flatten(), z_ak,s_k,f_k))
             self.minlp_obj.objective(x_vec)
         import pdb; pdb.set_trace()
+
+    def translate_minlp_objective(self, x):
+        x_ak, o_akk, z_ak, s_k, f_k = self.minlp_obj.partition_x(x)
+        # x_ak organized by agent
+        x_ak = np.reshape(np.array(x_ak), (self.num_robots, self.num_tasks + 1)) # reshape so each row contains x_ak for agent a
+        x_dummy = x_ak[:,0]
+        x_ak = x_ak[:,1:]
+        task_coalitions = []#np.zeros((self.num_tasks,)) # list of coalition size assigned to each task
+        for t in range(self.num_tasks):
+            task_coalitions.append(np.sum(x_ak[:,t]))
+        print("task coalitions: ",task_coalitions)
+        o_akk = np.atleast_3d(np.reshape(o_akk,(self.num_robots,self.num_tasks+1, self.num_tasks)))
+        breakpoint()
+        for a in range(self.num_robots):
+            for j in range(self.num_tasks+1):
+                for k in range(self.num_tasks):
+                    if(o_akk[a,j,k]>0.99):
+                        print("agent %d performs task %d before task %d" %(a,j,k))
+
+        tasks_ordered = np.argsort(np.array(f_k))
+        for t in tasks_ordered:
+            print("Time ", f_k[t],": task %d assigned %d agents" % (t, task_coalitions[t]))
 
 
 def discretize_pairwise(max_val):