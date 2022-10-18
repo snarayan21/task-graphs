2c922355c994d6aebebe71a902ec4c2ec3794bd9
Walker <wgosrich@gmail.com>
2022-10-18 11:42:10 -0400

    heatmap plotting tweaks

--- src/heatmap.py
+++ src/heatmap.py
@@ -16,21 +16,22 @@ def main():
             input_dict_list.append(json.load(loadfile))
 
     SMALL_SIZE = 10
-    MEDIUM_SIZE = 15
-    BIGGER_SIZE = 18
-    BIGGEST_SIZE = 25
+    MEDIUM_SIZE = 21
+    MED_BIG = 24
+    BIGGER_SIZE = 29
+    BIGGEST_SIZE = 36
 
     #### FONT SIZE ###########################################################
     plt.rc('font', size=MEDIUM_SIZE) # controls default text sizes
-    plt.rc('axes', titlesize=MEDIUM_SIZE) # fontsize of the axes title
-    plt.rc('axes', labelsize=BIGGER_SIZE) # fontsize of the x and y labels
+    plt.rc('axes', titlesize=BIGGER_SIZE) # fontsize of the axes title
+    plt.rc('axes', labelsize=MED_BIG) # fontsize of the x and y labels
     plt.rc('xtick', labelsize=MEDIUM_SIZE) # fontsize of the tick labels
     plt.rc('ytick', labelsize=MEDIUM_SIZE) # fontsize of the tick labels
     plt.rc('legend', fontsize=BIGGER_SIZE) # legend fontsize
     plt.rc('figure', titlesize=BIGGEST_SIZE) # fontsize of the figure title
 
     # FONT #################################
-    plt.rcParams["font.family"] = "Times New Roman"
+    #plt.rcParams["font.family"] = "Times New Roman"
     plt.rcParams["text.usetex"] = True
     plt.rcParams['text.latex.preamble'] = [r'\usepackage{amsmath}']
 
@@ -46,11 +47,11 @@ def main():
     flow_rewards_plot = np.array(flow_rewards)[sort_inds]
     greedy_rewards_plot = np.array(greedy_rewards)[sort_inds]
 
-    fig, axs = plt.subplots(1, 3, gridspec_kw={'width_ratios':[1,1,0.08]})
+    fig, axs = plt.subplots(1, 3, gridspec_kw={'width_ratios':[1,1,0.08], 'wspace':0.6})
     #fig.tight_layout(pad=0.5)
     plt.subplots_adjust(left=0.1, right=0.9,bottom=0.15,top=0.9, wspace=0.3)
-    g1 = sns.heatmap(flow_rewards_plot, linewidth=0.5, cmap='RdBu', center=1, xticklabels=[8, 12, 16, 20], yticklabels=[1.0, 0.75, 0.50, 0.25], ax=axs[0], vmin=0, vmax=5, cbar_ax=axs[2], cbar_kws={"label":"Normalized reward: flow NLP"}, square=True)
-    g2 = sns.heatmap(greedy_rewards_plot, linewidth=0.5, cmap='RdBu', cbar=False, center=1, xticklabels=[8, 12, 16, 20], yticklabels=[1.0, 0.75, 0.50, 0.25], ax=axs[1], vmin=0, vmax=5, square=True)# cbar_kws={"label":"%"}
+    g1 = sns.heatmap(flow_rewards_plot, linewidth=0.5, cmap='seismic_r', center=1, xticklabels=[8, 12, 16, 20], yticklabels=[1.0, 0.75, 0.50, 0.25], ax=axs[0], vmin=0, vmax=10, cbar_ax=axs[2], cbar_kws={"label":"Normalized Reward","pad":0,"extend":'max',"ticks":[0,1,2,4,6,8,10]}, square=True)
+    g2 = sns.heatmap(greedy_rewards_plot, linewidth=0.5, cmap='seismic_r', cbar=False, center=1, xticklabels=[8, 12, 16, 20], yticklabels=[1.0, 0.75, 0.50, 0.25], ax=axs[1], vmin=0, vmax=10, square=True)# cbar_kws={"label":"%"}
     #g3 = sns.heatmap(abs_radius, cmap='RdBu', cbar=True, xticklabels=[],yticklabels=[], center=0, cbar_ax=axs[2])
 
     g1.set_xlabel('Number of Tasks')
@@ -58,6 +59,9 @@ def main():
     g2.set_xlabel('Number of Tasks')
     g2.set_ylabel('Makespan Constraint')
 
+    g1.set_title('Flow NLP Reward (Norm)')
+    g2.set_title('Greedy Reward (Norm)')
+    #plt.subplots_adjust(wspace=1.0)
     plt.show()
 
 if __name__ == '__main__':
