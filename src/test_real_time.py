5a12e1a11756b6c9b6884a168ed8e4343069ebb6
Walker <wgosrich@gmail.com>
2024-02-26 01:23:04 -0500

    test updates

--- src/test_real_time.py
+++ src/test_real_time.py
@@ -3,7 +3,8 @@ import pathlib
 import toml
 import numpy as np
 #exp_filepath = '/home/walker/Documents/Work/task-graphs/src/experiment_data/tasks_iterative_1-10_10_exp_0/trial_4/args.toml'
-exp_filepath = '/home/walker/Documents/Work/task-graphs/src/experiment_data/real_time_test_0/trial_9/args.toml'
+#exp_filepath = '/home/walker/Documents/Work/task-graphs/src/experiment_data/real_time_test_0/trial_9/args.toml'
+exp_filepath = '/home/walker/Documents/Work/task-graphs/src/experiment_data/real_time_15_exp_91/trial_46/args.toml'
 exp_file = pathlib.Path(exp_filepath)
 all_args = toml.load(exp_filepath)
 
@@ -22,3 +23,5 @@ print(np.sum(real_time_solver.task_rewards))
 print("ORIGINAL REWARDS: ")
 print(real_time_solver.original_rewards)
 print(np.sum(real_time_solver.original_rewards))
+
+real_time_solver.print_trial_history()
